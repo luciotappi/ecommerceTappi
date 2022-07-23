6a543c27b6d57c0372a9538883a2b6f6a5f9d65a
luciotappi <lucio.tappi@qm-equipment.com>
2022-07-23 18:38:36 -0300

    navbar agregado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar'; // para importar funciones 
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <header className="App-header">
+      
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+      
     </div>
   );
 }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,69 @@
+// import Navbar from 'react-bootstrap/Navbar';
+
+
+
+// function  NavBar() {
+//     return (
+//         <Navbar expand="lg" variant="light" bg="light" fixed="top">
+
+//         </Navbar>
+          
+// );
+// }
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+import pic from ".//Tappi-logo.png";
+
+  function NavBar() {
+    return (
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+          <img
+              src={pic}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt=""
+            />
+             Tappi electronics</Navbar.Brand>
+          
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">  
+            <Nav className="me-auto">
+              <Nav.Link href="#features">Placas</Nav.Link>
+              <Nav.Link href="#features">Microcontroladores</Nav.Link>
+              <Nav.Link href="#features">Sensores</Nav.Link>
+              <Nav.Link href="#features">Electronica</Nav.Link>
+              <Nav.Link href="#pricing"></Nav.Link>
+              
+              <NavDropdown title="Soluciones" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Automatizacion</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  IOT
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Industria
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Personaliza tu solucion
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">Sobre Nosotros</Nav.Link>
+              <Nav.Link href="#deets">Contacto</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      
+    );
+  }
+
+export default NavBar;
