578537c41532e491e3189b5b3a2853b64ccf3804
luciotappi <lucio.tappi@qm-equipment.com>
2022-07-31 21:26:56 -0300

    Agregado ItemCount y ProductCard

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 import NavBar from './components/NavBar/NavBar'; // para importar funciones 
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'; // para importar funciones 
 
-
+import ItemCount from './components/ItemCount'; // no es necesario en este caso que el path sea ../ItemCount/ItemCount ya que el archivo .js se llama index.js
+import CardProduct from './components/CardProduct'; // para importar funciones 
 function App() {
   return (
     <div className="App">
       <NavBar/>
      
       <ItemListContainer greetings={'Pagina en construcción... Lamentamos las molestias! :('}/> 
-
+      {/* <ItemCount stock= '5' initial ='1'/> */}
     </div>
   );
 }
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,7 +1,16 @@
 
+import CardProduct from "../CardProduct";
 import pic from ".//under-construction.jpg";
 import pic2 from ".//under-construction2.jpg";
 
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+import imageUrl from '../CardProduct/arduino-uno.png';
+
+import ItemCount from "../ItemCount";
 
 const estilos = {
 
@@ -29,17 +38,53 @@ const Image = () => {
 
 function ItemListContainer(props) {
 
-  console.log('Estas son las props : ', props)
-
-
-    return(
-        
+  const cantItems =3;
+  const Data = [{
+    title: 'Arduino Uno',
+    description: 'Placa de desarrollo Arduino Uno basada en ATMega328P. Chip CH340 + Cable de programacion',
+    price:'$4000',
+    info:'https://arduino.cl/arduino-uno/', 
+    stock: 5,
+    initial:1
     
-  <div style= {estilos}>{props.greetings}
-  <br></br>
-  {/* <Image/> */}
-  </div>
-    );
+    
+  },
+  
+  {
+    title: 'ESP32',
+    description: 'Placa de desarollo ESP32 con procesador Tensilica Xtensa 32-bit LX6, antena Wi-Fi y receptor Bluetooth integrado',
+    price:'$3000',
+    info:'http://esp32.net/',
+    stock:32,
+    initial:1
+    
+    
+  },
+
+  {
+    title: 'STM32',
+    description: 'Placa de desarrollo STM32 Blue pill basada en STM32F103C8T6 STM32 Blue Pill CPU ARM 32 Cortex-M3',
+    price:'$2500',
+    info:'https://www.st.com/en/microcontrollers-microprocessors/stm32-32-bit-arm-cortex-mcus.html',
+    stock:0,
+    initial:1 
+    
+  }];
+
+  console.log(Data[0]);
+
+  return (
+    <Row xs={1} md={6} className="g-4 h-100 d-flex">
+    {Array.from({ length: cantItems }).map((_, idx) => (
+      <Col>
+        <CardProduct title={Data[idx].title} description={Data[idx].description} price={Data[idx].price} info={Data[idx].info} stock={Data[idx].stock} initial={Data[idx].initial}></CardProduct>
+      </Col>
+    ))}
+  </Row>
+    
+   
+  
+  );
 
 }
 
